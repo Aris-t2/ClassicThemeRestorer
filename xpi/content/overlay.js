568715a991afd806d358c25c599bfd623eada17b
Aris-t2 <aris-addons@gmx.net>
2015-05-30 11:09:03 +0200

    1.3.3 beta 5
    [locale] Greek (el) translation updated (thanks to George M.)
    
    [change] Windows + tabs not on top v1 + lw-themes + Fx38+
    --> (re)added missing line below tabs toolbar
    [change] additional note for 'Pocket' checkbox on preference window
    [change] minor pref window tweaks for RTL locales

--- xpi/content/overlay.js
+++ xpi/content/overlay.js
@@ -3474,6 +3474,27 @@ classicthemerestorerjs.ctr = {
 						  background-color: transparent !important;\
 						  background-image: url(chrome://browser/skin/tabbrowser/tab-active-middle.png),linear-gradient(transparent, transparent 2px,'+this.prefs.getCharPref('ctabact1')+' 2px, '+this.prefs.getCharPref('ctabact2')+'), none !important;\
 						}\
+						/* TabMixPlus fix */\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:not(:-moz-lwtheme) .tab-background-start[selected=true]:-moz-locale-dir(ltr),\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:not(:-moz-lwtheme) .tab-background-end[selected=true]:-moz-locale-dir(rtl) {\
+						  background-image: url(chrome://browser/skin/tabbrowser/tab-stroke-start.png),linear-gradient(transparent, transparent 2px,'+this.prefs.getCharPref('ctabact1')+' 2px, '+this.prefs.getCharPref('ctabact2')+') !important;\
+						  clip-path: url(chrome://browser/content/browser.xul#tab-curve-clip-path-start) !important;\
+						}\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:not(:-moz-lwtheme) .tab-background-end[selected=true]:-moz-locale-dir(ltr),\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:not(:-moz-lwtheme) .tab-background-start[selected=true]:-moz-locale-dir(rtl) {\
+						  background-image: url(chrome://browser/skin/tabbrowser/tab-stroke-end.png),linear-gradient(transparent, transparent 2px,'+this.prefs.getCharPref('ctabact1')+' 2px, '+this.prefs.getCharPref('ctabact2')+') !important;\
+						  clip-path: url(chrome://browser/content/browser.xul#tab-curve-clip-path-end) !important;\
+						}\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:-moz-lwtheme .tab-background-start[selected=true]:-moz-locale-dir(ltr),\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:-moz-lwtheme .tab-background-end[selected=true]:-moz-locale-dir(rtl) {\
+						  background: url(chrome://browser/skin/tabbrowser/tab-stroke-start.png),linear-gradient(transparent, transparent 2px,'+this.prefs.getCharPref('ctabact1')+' 2px, '+this.prefs.getCharPref('ctabact2')+') !important;\
+						  clip-path: url(chrome://browser/content/browser.xul#tab-curve-clip-path-start) !important;\
+						}\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:-moz-lwtheme .tab-background-end[selected=true]:-moz-locale-dir(ltr),\
+						#main-window #navigator-toolbox #TabsToolbar[currentset*="tabmixScrollBox"]:-moz-lwtheme .tab-background-start[selected=true]:-moz-locale-dir(rtl) {\
+						  background: url(chrome://browser/skin/tabbrowser/tab-stroke-end.png),linear-gradient(transparent, transparent 2px,'+this.prefs.getCharPref('ctabact1')+' 2px, '+this.prefs.getCharPref('ctabact2')+') !important;\
+						  clip-path: url(chrome://browser/content/browser.xul#tab-curve-clip-path-end) !important;\
+						}\
 						'+tabc_act_tb_sheet+'\
 					'), null, null);
 				
