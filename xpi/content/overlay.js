618209ebb0cbf3c2628d17f1bbcd18429336d6e1
Aris-t2 <aris-addons@gmx.net>
2017-05-15 19:44:31 +0200

    1.6.7beta2
    <b>[fix(es)]</b>
    - Fx55+ & 'movable back-forward buttons': location bar gap
    
    <b>[general change(s)]</b>
    - Fx55+: removed 'generic favicon' options (temporary)
    * Generic bookmarks favicon can not be changed for now

--- xpi/content/overlay.js
+++ xpi/content/overlay.js
@@ -2112,7 +2112,7 @@ classicthemerestorerjs.ctr = {
 			classicthemerestorerjs.ctr.loadUnloadCSS('emptyfavico_g_dot2',false);
 			classicthemerestorerjs.ctr.loadUnloadCSS('emptyfavico_g_sheet',false);
 			
-			if (branch.getCharPref("emptyfavico_g")!="emptyfavico_g_def"){
+			if (branch.getCharPref("emptyfavico_g")!="emptyfavico_g_def" &&  classicthemerestorerjs.ctr.appversion < 55){
 			  classicthemerestorerjs.ctr.loadUnloadCSS(branch.getCharPref("emptyfavico_g"),true);
 			}
 
